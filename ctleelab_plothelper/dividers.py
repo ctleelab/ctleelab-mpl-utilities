504c17acf5ec9a2d5e0179dbd66e04e2db09e551
Christopher Lee <ctlee@ucsd.edu>
2025-10-20 12:12:53 -0700

    Setting up Pixi and base utility files (#2)
    * Update gitignore
    
    Signed-off-by: Christopher T. Lee <ctlee@ucsd.edu>
    
    * Update spec; and add main drivers
    
    Signed-off-by: Christopher T. Lee <ctlee@ucsd.edu>
    
    * Add PR template
    
    Signed-off-by: Christopher T. Lee <ctlee@ucsd.edu>
    
    * Add misc files
    
    Signed-off-by: Christopher T. Lee <ctlee@ucsd.edu>
    
    * Update precommit
    
    Signed-off-by: Christopher T. Lee <ctlee@ucsd.edu>
    
    * Fix comments
    
    Signed-off-by: Christopher T. Lee <ctlee@ucsd.edu>
    
    * Fixes
    
    Signed-off-by: Christopher T. Lee <ctlee@ucsd.edu>
    
    ---------
    
    Signed-off-by: Christopher T. Lee <ctlee@ucsd.edu>

---
+++ ctleelab_plothelper/dividers.py
@@ -0,0 +1,64 @@
+#
+# ctleelab-mpl-utilities: A collection of utilities for plotting with matplotlib
+#
+# Copyright 2025- ctleelab
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Please help us support development by citing the research
+# papers on the package. Check out https://github.com/ctleelab/ctleelab-mpl-utilities/
+# for more information.
+
+
+import matplotlib as mpl
+import matplotlib.transforms as mtransforms
+from mpl_toolkits.axes_grid1 import Size
+from mpl_toolkits.axes_grid1.axes_divider import AxesDivider
+
+from typing import Callable, Any
+
+import functools
+
+
+class FixedSizeDivider(AxesDivider):
+    """
+    A custom AxesDivider that allows fixed size axes.
+
+    This class extends the AxesDivider to support fixed size axes in inches.
+    """
+
+    def __init__(self, axes, xref=None, yref=None):
+        super().__init__(axes, xref=xref, yref=yref)
+
+    def new_right_locator(self, pad: float, aspect: float) -> Callable:
+        """Locate a new axes to the RHS of current axes with explicit padding and width given in aspect to main figure.
+
+        Args:
+            pad (float): padding in inches to add between current axes and new axes
+            aspect (float): aspect of new axes relative to current
+
+        Returns:
+            Callable: locator functor
+        """
+        locator = functools.partial(self._locate, pad, aspect)
+        locator.get_subplotspec = self.get_subplotspec
+        return locator
+
+    def _locate(self, pad: float, aspect: float, axes: mpl.axes.Axes, renderer: Any):
+        """
+        Implementation of ``divider.new_locator().__call__``.
+
+        The axes locator callable returned by ``new_locator()`` is created as
+        a `functools.partial` of this method with *nx*, *ny*, *nx1*, and *ny1*
+        specifying the requested cell.
+        """
+        fig_w, fig_h = self._fig.bbox.size / self._fig.dpi
+        x0, y0, w, h = self.get_position_runtime(axes, renderer)
+
+        # width of new figure is given by aspect relative to width of current axis
+        x1, w1 = x0 + w + pad / fig_w, w / aspect
+        y1, h1 = y0, h
+
+        return mtransforms.Bbox.from_bounds(x1, y1, w1, h1)
--- ctleelab_plothelper/plothelpers.py
+++ ctleelab_plothelper/plothelpers.py
@@ -11,110 +11,193 @@
 # papers on the package. Check out https://github.com/ctleelab/ctleelab-mpl-utilities/
 # for more information.
 
-
-import importlib.util
-
-MPL_SPEC = importlib.util.find_spec("matplotlib")
-MPL_FOUND = MPL_SPEC is not None
-
-SNS_SPEC = importlib.util.find_spec("seaborn")
-SNS_FOUND = SNS_SPEC is not None
-
-if not MPL_FOUND:
-    raise RuntimeError(f"Plotting requires Matplotlib")
-if not SNS_FOUND:
-    raise RuntimeError(f"Plotting requires Seaborn")
+import numpy as np
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
-import matplotlib.lines as mlines
-import matplotlib.patches as mpatches
-import matplotlib.font_manager
 
-import seaborn as sns
-
-# print("Matplotlib Version:", mpl.__version__)
-
-# plt.style.use("seaborn-v0_8-colorblind")  # set plot style
-# mpl.rcParams["font.sans-serif"] = "Arial"
-# mpl.rcParams["font.family"] = "sans-serif"
-
-DARK_GREY = (0.1, 0.1, 0.1)
-THINNER_LINE = 0.5
-THIN_LINE = 0.7
-NORMAL_LINE = 0.8
-
-SMALL_SIZE = 8
-MEDIUM_SIZE = 10
-BIGGER_SIZE = 12
-FIG_DPI = 300
-
-plt.rc("font", size=SMALL_SIZE)  # controls default text sizes
-plt.rc("axes", titlesize=BIGGER_SIZE)  # fontsize of the axes title
-plt.rc("axes", labelsize=MEDIUM_SIZE)  # fontsize of the x and y labels
-plt.rc("xtick", labelsize=MEDIUM_SIZE)  # fontsize of the tick labels
-plt.rc("ytick", labelsize=MEDIUM_SIZE)  # fontsize of the tick labels
-plt.rc("legend", fontsize=SMALL_SIZE)  # legend fontsize
-plt.rc("figure", titlesize=BIGGER_SIZE)  # fontsize of the figure title
-plt.rcParams["figure.figsize"] = [3, 3]  # Default 3x3
-plt.rcParams["figure.dpi"] = FIG_DPI
-plt.rcParams["savefig.dpi"] = FIG_DPI
-plt.rcParams["axes.unicode_minus"] = False
-plt.rcParams["axes.labelpad"] = 2.5
-plt.rcParams["lines.markersize"] = 1
-
-# Force TrueType fonts
-plt.rcParams["pdf.fonttype"] = 42
-plt.rcParams["ps.fonttype"] = 42
-
-LinearWhiteBlueColormap = mpl.colors.LinearSegmentedColormap.from_list(
-    "LinearBlues", ["white", sns.color_palette()[0]], N=256
-)
-
-
-
-def matplotlibStyle(small: float = 6, medium: float = 8, large: float = 10):
-    """Set matplotlib plotting style
-
-    Args:
-        s (int, optional): Small size. Defaults to 6.
-        m (int, optional): Medium size. Defaults to 8.
-        l (int, optional): Large size. Defaults to 10.
-    """
-    plt.rcParams["font.sans-serif"] = "Arial"
-    plt.rcParams["font.family"] = "sans-serif"
-    plt.rcParams["lines.linewidth"] = 2
-    plt.rcParams["savefig.dpi"] = 600
-    # mpl.rcParams.update({'font.size': 8})
-    plt.rc("font", size=large)  # controls default text sizes
-    plt.rc("axes", titlesize=large)  # fontsize of the axes title
-    plt.rc("axes", labelsize=medium)  # fontsize of the x and y labels
-    plt.rc("xtick", labelsize=medium)  # fontsize of the tick labels
-    plt.rc("ytick", labelsize=medium)  # fontsize of the tick labels
-    plt.rc("legend", fontsize=small, frameon=False)  # legend fontsize
-    plt.rc("figure", titlesize=large)  # fontsize of the figure title
-    plt.rc("pdf", fonttype=42)
-
+# import matplotlib.lines as mlines
+# import matplotlib.patches as mpatches
+# import matplotlib.transforms as mtransforms
+# import matplotlib.font_manager
 
 from mpl_toolkits import axes_grid1
+from mpl_toolkits.axes_grid1 import Divider, Size
 
-from mpl_toolkits.axes_grid1.inset_locator import inset_axes, mark_inset # InsetPosition, mark_inset
+from .dividers import FixedSizeDivider
+
+from operator import sub
+
+import datetime
+
+import numpy.typing as npt
+from typing import Tuple, Union
 
 
-def add_colorbar(im, ax=None, aspect=20, pad_fraction=0.5, **kwargs):
-    """Add a vertical color bar to an image plot."""
+def fixed_size_subplots(
+    nrows: int = 1,
+    ncols: int = 1,
+    wmargin: float = 0.7,
+    hmargin: float = 0.7,
+    colsep: float = 0,
+    rowsep: float = 0,
+    subheight: float = 2,
+    subwidth: float = 2,
+    rmargin_scale: float = 0.6,
+    tmargin_scale: float = 0.6,
+    **fig_kw,
+) -> Tuple[plt.Figure, Union[mpl.axes.Axes, npt.NDArray[mpl.axes.Axes]]]:
+    """Utility wrapper for creating a figure with subplots of specific axes sizes.
+
+    All sizes are in inches. wmargin and hmargin are the width and height margins on the left and bottom sides.
+    The right and top margins are `wmargin` and `hmargin` scaled by `rmargin_scale` and `tmargin_scale`, respectively.
+    The spacing between subplots is given by `wmargin` and `hmargin` plus `rowsep` and `colsep`. For example, "| wmargin | subwidth | wmargin + colsep | subwidth | wmargin*rmargin_scale |"
+
+
+    Args:
+        nrows (int, optional): number of rows for subplot grid. Defaults to 1.
+        ncols (int, optional): number of columns for subplot grid. Defaults to 1.
+        wmargin (float, optional): size of width margins. Defaults to 0.7.
+        hmargin (float, optional): size of height margins. Defaults to 0.7.
+        colsep (float, optional): padding for column separation (width). Defaults to 0.
+        rowsep (float, optional): padding for row separation (height). Defaults to 0.
+        subheight (float, optional): subaxes height. Defaults to 2.
+        subwidth (float, optional): subaxes width. Defaults to 2.
+        rmargin_scale (float, optional): right margin scale factor. Defaults to 0.6.
+        tmargin_scale (float, optional): top margin scale factor. Defaults to 0.6.
+        **fig_kw: Additional keyword arguments passed to plt.figure()
+
+    Returns:
+        fig, axs (Tuple[plt.Figure, Tuple[mpl.axes.Axes, npt.NDArray[mpl.axes.Axes]]]):
+        *axs* can be either a single `mpl.axes.Axes` object, or an array of Axes
+        objects if more than one subplot was created.
+    """
+    width = ncols * (wmargin + colsep + subwidth) + wmargin * rmargin_scale
+    height = nrows * (hmargin + rowsep + subheight) + hmargin * tmargin_scale
+
+    axs = np.empty((nrows, ncols), dtype=object)
+
+    fig = plt.figure(figsize=(width, height), **fig_kw)
+    # renderer = get_renderer(fig)
+
+    # Initialize vertical divider locations
+    v = list()
+    for i in range(1, nrows + 1):
+        if i == 1:
+            v.append(Size.Fixed(hmargin))
+        else:
+            v.append(Size.Fixed(hmargin + rowsep))
+        v.append(Size.Fixed(subheight))
+
+    # Initialize horizontal divider locations
+    h = list()
+    for i in range(1, ncols + 1):
+        if i == 1:
+            h.append(Size.Fixed(wmargin))
+        else:
+            h.append(Size.Fixed(wmargin + colsep))
+        h.append(Size.Fixed(subwidth))
+
+    divider = Divider(fig, (0, 0, 1, 1), h, v, aspect=False)
+
+    for row in range(nrows):
+        for col in range(ncols):
+            axs[row, col] = fig.add_axes(
+                divider.get_position(),
+                axes_locator=divider.new_locator(nx=2 * col + 1, ny=2 * row + 1),
+            )
+    if axs.size == 1:
+        return fig, axs[0, 0]
+    return fig, np.squeeze(axs)
+
+
+def get_renderer(fig):
+    if hasattr(fig.canvas, "get_renderer"):
+        return fig.canvas.get_renderer()
+    elif hasattr(fig, "_get_renderer"):
+        return fig._get_renderer()
+    backend = matplotlib.get_backend()
+    raise AttributeError(f"Could not find a renderer for the '{backend}' backend.")
+
+
+def add_colorbar(
+    im: mpl.image.AxesImage,
+    ax: mpl.axes.Axes = None,
+    aspect: float = 20,
+    pad_fraction: float = 0.5,
+    **kwargs,
+) -> mpl.colorbar.Colorbar:
+    """Add a vertical color bar to an image plot.
+
+    Args:
+        im (mpl.image.AxesImage): The image to which the colorbar applies.
+        ax (mpl.axes.Axes, optional): The axes to draw the colorbar by. Defaults to None.
+        aspect (float, optional): Aspect width scaled to current axis. Defaults to 20.
+        pad_fraction (float, optional): Padding spacing. Defaults to 0.5.
+
+    Returns:
+        -> mpl.colorbar.Colorbar: Colorbar instance
+    """
     if ax is None:
         ax = plt.gca()
-    divider = axes_grid1.make_axes_locatable(im.axes)
-    width = axes_grid1.axes_size.AxesY(im.axes, aspect=1.0 / aspect)
+    divider = axes_grid1.make_axes_locatable(ax)
+    width = axes_grid1.axes_size.AxesY(ax, aspect=1.0 / aspect)
     pad = axes_grid1.axes_size.Fraction(pad_fraction, width)
     cax = divider.append_axes("right", size=width, pad=pad)
     plt.sca(ax)
     return im.axes.figure.colorbar(im, cax=cax, **kwargs)
 
 
-from operator import sub
+def add_fixed_colorbar(
+    im: mpl.image.AxesImage,
+    ax: mpl.axes.Axes = None,
+    aspect: float = 20,
+    pad: float = 0.05,
+    **kwargs,
+) -> mpl.colorbar.Colorbar:
+    """Add a vertical color bar to an axes with fixed non-floating subplots.
+
+    Args:
+        im (mpl.image.AxesImage): The image to which the colorbar applies.
+        ax (mpl.axes.Axes, optional): The axes to draw the colorbar by. Defaults to None.
+        aspect (float, optional): Aspect width in inches. Defaults to 20.
+        pad (float, optional): Padding spacing in inches. Defaults to 0.05.
+        **kwargs: Additional keyword arguments passed to colorbar().
+
+    Returns:
+        mpl.colorbar.Colorbar: Colorbar instance
+
+    """
+    if ax is None:
+        ax = plt.gca()
+
+    divider = FixedSizeDivider(ax)
+    divider.set_locator(ax.get_axes_locator())
+
+    cax = ax.figure.add_axes(
+        divider.get_position(),
+        axes_locator=divider.new_right_locator(pad, aspect),
+    )
+    plt.sca(cax)
+
+    # Hard-coded alternative which does not defer final placement until draw time.
+    # renderer = get_renderer(fig)
+
+    # divider = axes_grid1.axes_divider.AxesDivider(ax)
+    # divider.set_locator(ax.get_axes_locator())
+
+    # fig_w, fig_h = fig.bbox.size / fig.dpi
+
+    # print(divider.get_position())
+    # x0, y0, w, h = divider.get_position_runtime(ax, renderer)
+
+    # x1, w1 = x0 + w + pad / fig_w, w / aspect
+    # y1, h1 = y0, h
+
+    # bbx = mtransforms.Bbox.from_bounds(x1, y1, w1, h1)
+    # cax = fig.add_axes(bbx)
+    return im.axes.figure.colorbar(im, cax=cax, **kwargs)
 
 
 def get_aspect(ax):
@@ -131,128 +214,20 @@ def get_aspect(ax):
     return disp_ratio / data_ratio
 
 
-def plot_errorbar(means, stds, c, label):
-
-    plt.figure(figsize=(6, 4))
-    #     plt.rc('font', size=20)
-    #     plt.rc('xtick', labelsize=20)
-    #     plt.rc('ytick', labelsize=20)
-
-    color = c
-
-    plt.plot(means.index, means, c=color)
-    times_cur = means.index
-    yminus = means - stds
-    yplus = means + stds
-
-    plt.fill_between(
-        times_cur,
-        np.asarray(yminus),
-        np.asarray(yplus),
-        alpha=0.25,
-        edgecolor=color,
-        facecolor=color,
-        linewidth=1,
-        antialiased=True,
-    )
-
-    plt.xlabel("Time (s)")
-    plt.ylabel(label)
-
-    plt.tick_params(reset=True, color="black", direction="in", length=4)
-    plt.grid(False)
-
-    plt.tight_layout()
-
-
-#  to plot multiple
-def plot_multiple_errorbars(means, stds, c, label):
-
-    # You can call this multiple times to make several plots on top of each other.
-    #     plt.figure(figsize=(6,4))
-    #     plt.rc('font', size=20)
-    #     plt.rc('xtick', labelsize=20)
-    #     plt.rc('ytick', labelsize=20)
-
-    color = c
-
-    plt.plot(means.index, means, c=color)
-    times_cur = means.index
-    yminus = means - stds
-    yplus = means + stds
-
-    plt.fill_between(
-        times_cur,
-        np.asarray(yminus),
-        np.asarray(yplus),
-        alpha=0.25,
-        edgecolor=color,
-        facecolor=color,
-        linewidth=1,
-        antialiased=True,
-    )
-
-    plt.xlabel("Time (s)")
-    plt.ylabel(label)
-
-    plt.tick_params(reset=True, color="black", direction="in", length=4)
-    plt.grid(False)
-
-
-#     plt.tight_layout()
-# auto_fit the plot dimensions using plot tight layout
-def plot_multiple_errorbars_tight(means, stds, c, label):
-
-    # You can call this multiple times to make several plots on top of each other.
-
-    #     plt.figure(figsize=(6,4))
-    #     plt.rc('font', size=20)
-    #     plt.rc('xtick', labelsize=20)
-    #     plt.rc('ytick', labelsize=20)
-
-    color = c
-
-    plt.plot(means.index, means, c=color)
-    times_cur = means.index
-    yminus = means - stds
-    yplus = means + stds
-
-    plt.fill_between(
-        times_cur,
-        np.asarray(yminus),
-        np.asarray(yplus),
-        alpha=0.25,
-        edgecolor=color,
-        facecolor=color,
-        linewidth=1,
-        antialiased=True,
-    )
-
-    plt.xlabel("Time (s)")
-    plt.ylabel(label)
-
-    plt.tick_params(reset=True, color="black", direction="in", length=4)
-    plt.grid(False)
-
-    plt.tight_layout()
-
-
 def custom_cmap(size, label, ticklabels, cmap=cm.RdPu):
     """
     Function to define colormap
     """
     cmap = cm.RdPu  # define the colormap
     cmaplist = [cmap(i) for i in range(cmap.N)]
-    cmap = mplt.colors.LinearSegmentedColormap.from_list(
-        "Custom cmap", cmaplist, cmap.N
-    )
+    cmap = mpl.colors.LinearSegmentedColormap.from_list("Custom cmap", cmaplist, cmap.N)
     bounds = np.linspace(0, 1, size)
-    norm = mplt.colors.BoundaryNorm(bounds, cmap.N)
+    norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
     val = (float(bounds[1]) - float(bounds[0])) / 2
     ticks = np.linspace(val, 1 - val, size - 1)
 
     cax = fig.add_axes([0.92, 0.25, 0.02, 0.5])
-    cb = mplt.colorbar.ColorbarBase(
+    cb = mpl.colorbar.ColorbarBase(
         cax,
         cmap=cmap,
         norm=norm,
@@ -265,12 +240,11 @@ def custom_cmap(size, label, ticklabels, cmap=cm.RdPu):
     cb.ax.set_yticklabels(ticklabels)  # vertically oriented colorbar
 
 
-import datetime
-
 now = datetime.datetime.now()
 date = now.strftime("%Y%m%d")
 
 
 def save_fig(fig, basename: str):
     fig.savefig(f"{basename}.png", format="png")
+    fig.savefig(f"{basename}.svg", format="svg")
     fig.savefig(f"{basename}.pdf", format="pdf")
